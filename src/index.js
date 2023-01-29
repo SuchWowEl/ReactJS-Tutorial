bb7345a69e1366aa50b90a45535ecd606691cf35
Elizer Dolorosa <zykisriel0434@gmail.com>
2023-01-30 00:18:39 +0800

    working game but needs fix

--- src/index.js
+++ src/index.js
@@ -2,23 +2,84 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+/* comment subclass, then transform into what we call "function component"
 class Square extends React.Component {
-    render() {
-      return (
-        <button className="square">
-          {/* TODO */}
-        </button>
-      );
-    }
+  render() {
+    return (
+      <button 
+        className = "square" 
+        onClick = {() => this.props.onClick()}>
+        {this.props.value}
+      </button>
+    );
+  }
+}*/
+  //function component
+  function Square(props){
+    return (
+      <button className = "square" onClick = {props.onClick}>
+        {props.value}
+      </button>
+    );
   }
   
   class Board extends React.Component {
+    constructor(props){
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null),
+        xTurn: true,
+      };
+    }
+
+    determineWinner(squares){
+      let full = "TIED";
+      //column check
+      for(let i = 0; i <= 2; i++){
+        if(squares[i] == null) full = false;
+        if(squares[i] === squares[i+3] && squares[i+3] === squares[i+6]) return squares[i] ;
+      }
+      //row check
+      for(let i = 0; i <= 6; i += 3){
+        if(squares[i] == null) full = false;
+        if(squares[i] === squares[i+1] && squares[i+1] === squares[i+2]) return squares[i];
+      }
+      //diagonal check
+      for(let i = 8; i >= 6; i -= 2){
+        if(squares[i] == null) full = false;
+        if(squares[i] === squares[4] && squares[4] === squares[i%4]) return squares[i];
+      }
+      return full;
+    }
+
+    handleClick(i){
+      const squares = this.state.squares.slice();
+      if (this.determineWinner(squares)){
+        this.setState({
+          squares: Array(9).fill(null),
+          xTurn: true
+        });
+      }
+      else if (squares[i] == null){
+        squares[i] = (this.state.xTurn) ? 'X' : 'O'; //good old if-else can also be used
+        this.setState({
+          squares: squares,
+          xTurn: !this.state.xTurn
+        });
+      }  
+    }
+
     renderSquare(i) {
-      return <Square />;
+      return <Square 
+                value = {this.state.squares[i]} 
+                onClick = {() => this.handleClick(i)}
+              />;
     }
   
     render() {
-      const status = 'Next player: X';
+      const winner = this.determineWinner(this.state.squares);
+      let status = (winner) ? winner + ' 🤯🤯🤯': 'Current player: ' + (this.state.xTurn ? 'X' : 'O');
+      //const status = 'Current player: ' + (this.state.xTurn ? 'X' : 'O');
   
       return (
         <div>
